3b813f9bd99a2f7bac886e298caef7816e23ee15
TriUnityTech <contato@triunitytech.com.br>
2024-07-13 13:54:50 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
   return (
     <div className={`App ${bgLoaded ? '' : 'hidden'}`}>
       <header className="App-header">
-        <div className="flex items-start justify-center bg-black bg-cover bg-center" style={{ backgroundImage: isMobile ? '' : "url('/bg.png')" }}>
+        <div className="flex items-start justify-center bg-black bg-cover bg-center">
           {!bgLoaded && (
             <div className="loader">Loading...</div>
           )}
@@ -33,7 +33,7 @@ function App() {
               <motion.img 
                 src='/img.png'
                 alt="Adv" 
-                className='w-112' 
+                className='lg:w-2/6 w-full ' 
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
                 transition={{ duration: 4 }}
@@ -42,14 +42,14 @@ function App() {
               <motion.img 
                 src='/logo.svg' 
                 alt="Logo" 
-                className='w-64 -mt-11 sm:-mt-14 z-50 '
+                className='w-64 sm:w-96 -mt-11 sm:-mt-14 z-50 '
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
                 transition={{ duration: 2 }}
               />
               
               <motion.p
-                className='text-custom text-sm py-4  tracking-widest' 
+                className='text-custom text-sm sm:text-lg py-4  tracking-widest' 
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
                 transition={{ duration: 2 }}
@@ -57,56 +57,61 @@ function App() {
                 DESIGNER DE SOBRANCELHA
               </motion.p>
               <BtnLinks />
-              <div className='mt-6 flex flex-col gap-4'>
+              <div className='mt-6 justify-center items-center px-5 sm:px-20 flex flex-col gap-4'>
                 <motion.img 
                   src='/link1.png'
                   alt="Link 1" 
-                  className='w-80 cursor-pointer' 
+                  className='w-full lg:w-2/6 cursor-pointer' 
                   initial={{ x: -100, opacity: 0 }}
                   animate={{ x: 0, opacity: 1 }}
                   transition={{ duration: 2, delay: 1 }} 
                   whileHover={{ scale: 1.1 }}
                   whileTap={{ scale: 0.9 }}
+                  // onClick={() => window.location.href = '/link-1'}
                 />
                 <motion.img 
                   src='/link2.png'
                   alt="Link 2" 
-                  className='w-80 cursor-pointer' 
+                  className='w-full lg:w-2/6 cursor-pointer' 
                   initial={{ x: -100, opacity: 0 }}
                   animate={{ x: 0, opacity: 1 }}
                   transition={{ duration: 2, delay: 1 }} 
                   whileHover={{ scale: 1.1 }}
                   whileTap={{ scale: 0.9 }}
+                  onClick={() => window.location.href = 'https://wa.me/5537988118274?text=Ol%C3%A1%2C%20gostaria%20de%20agendar%20um%20hor%C3%A1rio'}
                 />
                 <motion.img 
                   src='/link3.png'
                   alt="Link 3" 
-                  className='w-80 cursor-pointer' 
+                  className='w-full lg:w-2/6 cursor-pointer' 
                   initial={{ x: -100, opacity: 0 }}
                   animate={{ x: 0, opacity: 1 }}
                   transition={{ duration: 2, delay: 1 }} 
                   whileHover={{ scale: 1.1 }}
                   whileTap={{ scale: 0.9 }}
+                  onClick={() => window.location.href = 'https://wa.me/5537988118274?text=Ol%C3%A1%2C%20gostaria%20de%20saber%20mais%20informa%C3%A7%C3%B5es%20sobre%20seu%20curso%20presencial'}
                 />
                 <motion.img 
                   src='/link4.png'
                   alt="Link 4" 
-                  className='w-80 cursor-pointer' 
+                  className='w-full lg:w-2/6 cursor-pointer' 
                   initial={{ x: -100, opacity: 0 }}
                   animate={{ x: 0, opacity: 1 }}
                   transition={{ duration: 2, delay: 1 }} 
                   whileHover={{ scale: 1.1 }}
                   whileTap={{ scale: 0.9 }}
+                  onClick={() => window.location.href = 'https://www.instagram.com/cintiaresendedesigner?igsh=aXd6Zmpid2FqdWp4'}
                 />
                 <motion.img 
                   src='/link5.png'
                   alt="Link 5" 
-                  className='w-80 cursor-pointer' 
+                  className='w-full lg:w-2/6 cursor-pointer' 
                   initial={{ x: -100, opacity: 0 }}
                   animate={{ x: 0, opacity: 1 }}
                   transition={{ duration: 2, delay: 1 }} 
                   whileHover={{ scale: 1.1 }}
                   whileTap={{ scale: 0.9 }}
+                  // onClick={() => window.location.href = '/link-5'}
                 />
               </div>
               <div className='py-8'>
